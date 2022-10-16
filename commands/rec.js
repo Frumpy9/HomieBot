a0712950679ede8c3f454e07ad0e96ae596b8602
synctax <benwyngaard@gmail.com>
2022-10-16 18:03:30 -0400

    added rec-song test

---
+++ commands/rec.js
@@ -0,0 +1,13 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('rec-song')
+        .setDescription('Create song recommendation')
+        .addStringOption((option) => option.setName("Name").setDescription("The name of the song").setRequired(true)),
+    async excecute(interaction) {
+        const name = interaction.options.getString("Name");
+
+        await interaction.reply(`mf said ${name}. lol`)
+    }
+}
