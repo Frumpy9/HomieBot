98c992244c4ce5048c69085804d8045db6df4ced
Frumpy9 <32330391+Frumpy9@users.noreply.github.com>
2022-10-16 18:28:03 -0400

    typo

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientID, guildID, token } = require('./config.json');
 
 const commands = []
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"))//gets all commands in /commands
@@ -19,7 +19,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 (async () => {
 	try {
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(clientID, guildID),
 			{ body: commands },
 		);
 
