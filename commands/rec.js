85880a79e9014fb444a816b50b35a0ee474e7a6b
synctax <benwyngaard@gmail.com>
2022-10-16 18:25:27 -0400

    fixed absolutely braindead problem

--- commands/rec.js
+++ commands/rec.js
@@ -4,9 +4,9 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('rec-song')
         .setDescription('Create song recommendation')
-        .addStringOption((option) => option.setName("Name").setDescription("The name of the song").setRequired(true)),
+        .addStringOption((option) => option.setName("name").setDescription("The name of the song").setRequired(true)),
     async excecute(interaction) {
-        const name = interaction.options.getString("Name");
+        const name = interaction.options.getString("name");
 
         await interaction.reply(`mf said ${name}. lol`)
     }
