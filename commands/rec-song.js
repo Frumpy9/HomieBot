ac2b7efec709ada9a6581e540fa886c44a63f354
Frumpy9 <32330391+Frumpy9@users.noreply.github.com>
2022-10-17 00:41:29 -0400

    format

--- commands/rec-song.js
+++ commands/rec-song.js
@@ -1,103 +1,104 @@
-const { SlashCommandBuilder, ThreadAutoArchiveDuration, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const { spotifyApi } = require('../spotify');
-const { getAverageColor } = require('fast-average-color-node');
-
+const { SlashCommandBuilder, ThreadAutoArchiveDuration, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { spotifyApi } = require("../spotify");
+const { getAverageColor } = require("fast-average-color-node");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('rec-song')
-        .setDescription('Create song recommendation')
-        .addStringOption((option) => option.setName("url").setDescription("url of song").setRequired(true)),
-    async execute(interaction) {
-        const url = interaction.options.getString("url");
-        const forum = interaction.guild.channels.cache.get('1031337709568020600');
+	data: new SlashCommandBuilder()
+		.setName("rec-song")
+		.setDescription("Create song recommendation")
+		.addStringOption((option) => option.setName("url").setDescription("url of song").setRequired(true)),
+	async execute(interaction) {
+		const url = interaction.options.getString("url");
+		const forum = interaction.guild.channels.cache.get("1031337709568020600");
 
-        const songID = url.split('/').pop().split('?')[0];
-        let songData;
+		const songID = url.split("/").pop().split("?")[0];
+		let songData;
 
-        await spotifyApi.getTrack(songID).then(
-            (data) => {
-                songData = data.body
-            },
-            async (err) => {
-                await interaction.reply({ content: "Couldn't find song!", ephemeral: true })
-                console.error(err);
-                return;
-            }
-        );
+		await spotifyApi.getTrack(songID).then(
+			(data) => {
+				songData = data.body;
+			},
+			async (err) => {
+				await interaction.reply({ content: "Couldn't find song!", ephemeral: true });
+				console.error(err);
+				return;
+			}
+		);
 
-        const color = await getAverageColor(songData.album.images[2].url);
-        let threadId;
+		const color = await getAverageColor(songData.album.images[2].url);
+		let threadId;
 
-        await forum.threads.create({
-            name: songData.name + ' - ' + songData.artists[0].name,
-            autoArchiveDuration: ThreadAutoArchiveDuration.OneDay,
-            message: {
-                content: interaction.member.toString() + ' recommended **' + songData.name + ' by ' + 
-                songData.artists[0].name + '**\n' + songData.external_urls.spotify,
-            },
-            reason: 'Song recommended through HomieBot by: ' + interaction.member.displayName,
-        })
-        .then((t) => {
-            const row = new ActionRowBuilder().addComponents( new ButtonBuilder()
-                .setCustomId('rec-song#'+songData.artists[0].external_urls.spotify)
-				.setLabel('More songs from ' + songData.artists[0].name)
-				.setStyle(ButtonStyle.Secondary),
-            )
-            const embed = createSongEmbed(songData, interaction.member).setColor(color.hex);
-            threadId = t.id;
-            t.send({embeds: [embed], content: '', components: [row]});
-        })
-        .catch(console.error);
+		await forum.threads
+			.create({
+				name: songData.name + " - " + songData.artists[0].name,
+				autoArchiveDuration: ThreadAutoArchiveDuration.OneDay,
+				message: {
+					content:
+						interaction.member.toString() +
+						" recommended **" +
+						songData.name +
+						" by " +
+						songData.artists[0].name +
+						"**\n" +
+						songData.external_urls.spotify
+				},
+				reason: "Song recommended through HomieBot by: " + interaction.member.displayName
+			})
+			.then((t) => {
+				const row = new ActionRowBuilder().addComponents(
+					new ButtonBuilder()
+						.setCustomId("rec-song#" + songData.artists[0].external_urls.spotify)
+						.setLabel("More songs from " + songData.artists[0].name)
+						.setStyle(ButtonStyle.Secondary)
+				);
+				const embed = createSongEmbed(songData, interaction.member).setColor(color.hex);
+				threadId = t.id;
+				t.send({ embeds: [embed], content: "", components: [row] });
+			})
+			.catch(console.error);
 
-
-
-        await interaction.reply({ content: 'Created song recommendation: <#' + threadId + '>', ephemeral: true })
-    },
-    async onButton(interaction, params) {
-        const url = params[0];
-        await interaction.reply({ content: url, ephemeral: true })
-    }
-}
+		await interaction.reply({ content: "Created song recommendation: <#" + threadId + ">", ephemeral: true });
+	},
+	async onButton(interaction, params) {
+		const url = params[0];
+		await interaction.reply({ content: url, ephemeral: true });
+	}
+};
 
 const createSongEmbed = (data, member) => {
-    const {album, name, external_urls, artists, duration_ms} = data;
-    // const {displayName, displayAvatarURL} = member;
+	const { album, name, external_urls, artists, duration_ms } = data;
+	// const {displayName, displayAvatarURL} = member;
 
-    const embed = new EmbedBuilder()
-    .setAuthor({name: member.displayName, iconURL: member.displayAvatarURL()})
-    .setTitle(name + ' by ' + artists[0].name)
-    .setURL(external_urls.spotify)
-    .setThumbnail(album.images[2].url)
-    .addFields([
-            {
-                "name": "Artists",
-                "value": artists.map((artist) => "["+artist.name+"]("+artist.external_urls.spotify+")").join('\n'),
-                "inline": true
-            },
-            {
-                "name": "Album",
-                "value": "["+album.name+"]("+album.external_urls.spotify+")",
-                "inline": true
-            },
-            {
-                "name": "Duration",
-                "value": millisToMinutesAndSeconds(duration_ms),
-                "inline": false
-            }
-    ])
-    .setFooter({text: 'released'})
-    .setTimestamp(new Date(album.release_date));
+	const embed = new EmbedBuilder()
+		.setAuthor({ name: member.displayName, iconURL: member.displayAvatarURL() })
+		.setTitle(name + " by " + artists[0].name)
+		.setURL(external_urls.spotify)
+		.setThumbnail(album.images[2].url)
+		.addFields([
+			{
+				name: "Artists",
+				value: artists.map((artist) => "[" + artist.name + "](" + artist.external_urls.spotify + ")").join("\n"),
+				inline: true
+			},
+			{
+				name: "Album",
+				value: "[" + album.name + "](" + album.external_urls.spotify + ")",
+				inline: true
+			},
+			{
+				name: "Duration",
+				value: millisToMinutesAndSeconds(duration_ms),
+				inline: false
+			}
+		])
+		.setFooter({ text: "released" })
+		.setTimestamp(new Date(album.release_date));
 
-    return embed;
-}
+	return embed;
+};
 
 function millisToMinutesAndSeconds(millis) {
-    var minutes = Math.floor(millis / 60000);
-    var seconds = ((millis % 60000) / 1000).toFixed(0);
-    return (
-        seconds == 60 ?
-        (minutes+1) + ":00" :
-        minutes + ":" + (seconds < 10 ? "0" : "") + seconds
-      );
-  }
+	var minutes = Math.floor(millis / 60000);
+	var seconds = ((millis % 60000) / 1000).toFixed(0);
+	return seconds == 60 ? minutes + 1 + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,31 +1,28 @@
 module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction) {
-        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+	name: "interactionCreate",
+	async execute(interaction) {
+		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
 
-        
+		if (interaction.isCommand()) {
+			const command = interaction.client.commands.get(interaction.commandName);
+			try {
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(error);
+				await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+			}
+		}
 
-        if (interaction.isCommand()) {
-            const command = interaction.client.commands.get(interaction.commandName);
-            try {
-                await command.execute(interaction);
-            } catch (error) {
-                console.error(error);
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
-        }
-
-        if (interaction.isButton()) {
-            const customIdSeperated = interaction.customId.split('#');
-            const command = interaction.client.commands.get(customIdSeperated[0]);
-            try {
-                customIdSeperated.shift();
-                command.onButton(interaction, customIdSeperated);
-            } catch (error) {
-                console.error(error);
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
-        }
-
-    },
+		if (interaction.isButton()) {
+			const customIdSeperated = interaction.customId.split("#");
+			const command = interaction.client.commands.get(customIdSeperated[0]);
+			try {
+				customIdSeperated.shift();
+				command.onButton(interaction, customIdSeperated);
+			} catch (error) {
+				console.error(error);
+				await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+			}
+		}
+	}
 };
