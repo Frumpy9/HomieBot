c1b889494847028f92325cad37a5790f9565e6c4
synctax <benwyngaard@gmail.com>
2022-10-16 23:21:23 -0400

    added average color to embed

--- commands/rec-song.js
+++ commands/rec-song.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, ThreadAutoArchiveDuration, EmbedBuilder } = require('discord.js');
 const { spotifyApi } = require('../spotify');
+const { getAverageColor } = require('fast-average-color-node');
 
 
 module.exports = {
@@ -15,13 +16,17 @@ module.exports = {
         let songData;
 
         await spotifyApi.getTrack(songID).then(
-            function (data) {
+            (data) => {
                 songData = data.body
             },
-            function (err) {
+            async (err) => {
+                await interaction.reply({ content: "Couldn't find song!", ephemeral: true })
                 console.error(err);
+                return;
             }
         );
+
+        const color = await getAverageColor(songData.album.images[2].url);
 
         await forum.threads.create({
             name: songData.name + ' -- ' + interaction.member.displayName,
@@ -33,46 +38,7 @@ module.exports = {
             reason: 'Needed a separate thread for food',
         })
         .then((t) => {
-            //let embed = new EmbedBuilder().setDescription('anything in there');
-            let embed = {
-                "title": songData.name+ " by " + songData.artists[0].name,
-                "description": "",
-                "color": 48028,
-                "timestamp": new Date(songData.album.release_date),
-                "url": songData.external_urls.spotify,
-                "author": {
-                    "name": interaction.member.displayName,
-                    //"url": "https://discord.com",
-                    "icon_url": interaction.member.displayAvatarURL()
-                },
-                "thumbnail": {
-                    "url": songData.album.images[2].url
-                },
-                // "image": {
-                //     "url": ""
-                // },
-                "footer": {
-                    "text": "Released",
-                    //"icon_url": ""
-                },
-                "fields": [
-                    {
-                        "name": "Artists",
-                        "value": songData.artists.map((artist) => "["+artist.name+"]("+artist.external_urls.spotify+")").join('\n'),
-                        "inline": true
-                    },
-                    {
-                        "name": "Album",
-                        "value": "["+songData.album.name+"]("+songData.album.external_urls.spotify+")",
-                        "inline": true
-                    },
-                    {
-                        "name": "Duration",
-                        "value": millisToMinutesAndSeconds(songData.duration_ms),
-                        "inline": false
-                    }
-                ]
-            }
+            const embed = createSongEmbed(songData, interaction.member).setColor(color.hex);
             t.send({embeds: [embed], content: ''});
         })
         .catch(console.error);
@@ -81,6 +47,38 @@ module.exports = {
 
         await interaction.reply({ content: `mf said ${songData.name}. lol`, ephemeral: true })
     }
+}
+
+const createSongEmbed = (data, member) => {
+    const {album, name, external_urls, artists, duration_ms} = data;
+    // const {displayName, displayAvatarURL} = member;
+
+    const embed = new EmbedBuilder()
+    .setAuthor({name: member.displayName, iconURL: member.displayAvatarURL()})
+    .setTitle(name + ' by ' + artists[0].name)
+    .setURL(external_urls.spotify)
+    .setThumbnail(album.images[2].url)
+    .addFields([
+            {
+                "name": "Artists",
+                "value": artists.map((artist) => "["+artist.name+"]("+artist.external_urls.spotify+")").join('\n'),
+                "inline": true
+            },
+            {
+                "name": "Album",
+                "value": "["+album.name+"]("+album.external_urls.spotify+")",
+                "inline": true
+            },
+            {
+                "name": "Duration",
+                "value": millisToMinutesAndSeconds(duration_ms),
+                "inline": false
+            }
+    ])
+    .setFooter({text: 'released'})
+    .setTimestamp(new Date(album.release_date));
+
+    return embed;
 }
 
 function millisToMinutesAndSeconds(millis) {
